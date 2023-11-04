dc5f11fb74fa30b7fe256e9fdcb17f2159340a92
Dominik Drexler <dominik.drexler@gmx.net>
2023-11-04 11:54:58 +0100

    tests

--- tests/domain/function_type.cpp
+++ tests/domain/function_type.cpp
@@ -8,20 +8,27 @@
 namespace loki::domain::tests {
 
 TEST(LokiTests, FunctionTypeTest) {
-    // A function type is just a number
     ast::FunctionType ast;
 
+    // Number (int)
     EXPECT_NO_THROW(parse_ast("5", function_type(), ast));
     EXPECT_EQ(parse_text(ast), "5");
-    EXPECT_NO_THROW(parse_ast("4.2", function_type(), ast));
-    EXPECT_EQ(parse_text(ast), "4.2");
     EXPECT_NO_THROW(parse_ast("6 7", function_type(), ast));
     EXPECT_EQ(parse_text(ast), "6");
     // TODO: Is this really what we want?
     EXPECT_NO_THROW(parse_ast("1loki", function_type(), ast));
     EXPECT_EQ(parse_text(ast), "1");
 
-    EXPECT_ANY_THROW(parse_ast("loki", function_type(), ast));
+    // Number (float)
+    EXPECT_NO_THROW(parse_ast("4.2", function_type(), ast));
+    EXPECT_EQ(parse_text(ast), "4.2");
+
+    // Type
+    EXPECT_NO_THROW(parse_ast("loki", function_type(), ast));
+    EXPECT_EQ(parse_text(ast), "loki");
+    EXPECT_NO_THROW(parse_ast("(either type1 type2)", function_type(), ast));
+    EXPECT_EQ(parse_text(ast), "(either type1 type2)");
+
     EXPECT_ANY_THROW(parse_ast("(5)", function_type(), ast));
 }
 
