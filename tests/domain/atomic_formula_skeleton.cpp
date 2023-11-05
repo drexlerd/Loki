74a9647b0e1835b2dd424ba3f11ad16d554c6b77
Dominik Drexler <dominik.drexler@gmx.net>
2023-11-05 13:43:48 +0100

    .

--- tests/domain/atomic_formula_skeleton.cpp
+++ tests/domain/atomic_formula_skeleton.cpp
@@ -3,6 +3,7 @@
 #include "include/loki/common/parser_wrapper.hpp"
 #include "include/loki/domain/parser.hpp"
 #include "include/loki/domain/printer.hpp"
+#include "tests/common/printer.hpp"
 
 
 namespace loki::domain::tests {
@@ -11,13 +12,13 @@ TEST(LokiTests, AtomicFormulaSkeletonTest) {
     ast::AtomicFormulaSkeleton ast;
 
     EXPECT_NO_THROW(parse_ast("(predicate1 ?var1 ?var2)", atomic_formula_skeleton(), ast));
-    EXPECT_EQ(parse_text(ast), "(predicate1 ?var1 ?var2)");
+    EXPECT_EQ(parse_text(ast, get_test_options()), "(predicate1 ?var1 ?var2)");
 
     EXPECT_NO_THROW(parse_ast("(predicate1 ?var1 - type1 ?var2 - type2)", atomic_formula_skeleton(), ast));
-    EXPECT_EQ(parse_text(ast), "(predicate1 ?var1 - type1\n?var2 - type2)");
+    EXPECT_EQ(parse_text(ast, get_test_options()), "(predicate1 ?var1 - type1\n?var2 - type2)");
 
     EXPECT_NO_THROW(parse_ast("(predicate1 ?var1 ?var2 - type1)", atomic_formula_skeleton(), ast));
-    EXPECT_EQ(parse_text(ast), "(predicate1 ?var1 ?var2 - type1)");
+    EXPECT_EQ(parse_text(ast, get_test_options()), "(predicate1 ?var1 ?var2 - type1)");
 
     EXPECT_ANY_THROW(parse_ast("(?var1 ?var2 - type1)", atomic_formula_skeleton(), ast));
 }
