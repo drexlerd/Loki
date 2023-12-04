0bbe73066ef1b2423c521c192ba87733178053f2
Dominik Drexler <dominik.drexler@gmx.net>
2023-12-04 17:45:16 +0100

    work on scope

--- include/loki/domain/pddl/context.hpp
+++ include/loki/domain/pddl/context.hpp
@@ -21,14 +21,72 @@
 
 #include "../../common/factory.hpp"
 
+#include <cassert>
 #include <deque>
 #include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include <tuple>
 
 
 namespace loki::domain {
+    template<typename... Ts>
+    class Bindings {
+        private:
+            std::tuple<std::unordered_map<std::string, std::shared_ptr<const Ts>>...> bindings;
+
+        public:
+            /// @brief Gets a binding of type T. Returns nullptr if it does not exist.
+            template<typename T> 
+            std::shared_ptr<const T> get(const std::string& key) const {
+                const auto& t_bindings = std::get<T>(bindings);
+                auto it = t_bindings.find(key);
+                if (it != t_bindings.end()) {
+                    return it->second;
+                }
+                return nullptr;
+            }
+
+            /// @brief Inserts a binding of type T
+            template<typename T>
+            void insert(const std::string& key, std::shared_ptr<const T> binding) {
+                auto& t_bindings = std::get<T>(bindings);
+                assert(!t_bindings.count(key));
+                t_bindings.emplace(key, binding);
+            }
+    };
+
     /// @brief Contains scoped bindings
-    struct Scope {
-        std::unordered_map<std::string, pddl::Variable> variables;
+    class Scope {
+        private:
+            std::shared_ptr<const Scope> m_parent_scope;
+
+            Bindings<pddl::TypeImpl
+                , pddl::ObjectImpl
+                , pddl::PredicateImpl 
+                , pddl::FunctionSkeletonImpl
+                , pddl::VariableImpl> bindings;
+
+        public:
+            explicit Scope(std::shared_ptr<const Scope> parent_scope = nullptr) 
+                : m_parent_scope(parent_scope) { }
+
+            /// @brief Resolves the name. Returns nullptr if the name could not be resolved.
+            template<typename T>
+            std::shared_ptr<const T> get(const std::string& name) const {
+                auto result = bindings.get<T>(name);
+                if (result) return result;
+                if (m_parent_scope) return this->get<T>(name);
+                return nullptr;
+            }
+
+            /// @brief Insert binding of type T.
+            template<typename T>
+            void insert(const std::string& name, std::shared_ptr<const T> binding) {
+                assert(!this->get<T>(name));
+                bindings.insert<T>(name, binding);
+            }
     };
 
 
@@ -56,34 +114,25 @@ namespace loki::domain {
             , pddl::ActionImpl
             , pddl::DerivedPredicateImpl
             , pddl::DomainImpl> cache;
-        // Requirements for testing
+
+        std::deque<std::shared_ptr<Scope>> scopes;   
+
+        // Singletons for convenience.
         pddl::Requirements requirements;
-
-        std::unordered_map<std::string, pddl::Type> types_by_name;
-        std::unordered_map<std::string, pddl::Object> constants_by_name;
-        std::unordered_map<std::string, pddl::Predicate> predicates_by_name;
-        std::unordered_map<std::string, pddl::FunctionSkeleton> function_skeletons_by_name;
-
-        // If true, the parsed variables are required to be in the set of defined variables.
-        bool require_defined_variables;
-        // The variables that are defined in the current scope
-        std::unordered_set<pddl::Variable> defined_variables;
-
-        // Nested variable bindings
-        std::deque<Scope> scopes;
-
-        // For convenience
         pddl::Type base_type_object;
         pddl::Type base_type_number;
 
         Context(std::unique_ptr<std::ostringstream>&& error_stream_)
-            : error_stream(std::move(error_stream_)),
-              require_defined_variables(false) {
+            : error_stream(std::move(error_stream_)) {
+            // Initialize the global scope
+            auto global_scope = std::make_shared<Scope>(nullptr);
+            scopes.push_back(global_scope);
+
             // create base types.
             base_type_object = cache.get_or_create<pddl::TypeImpl>("object").object;
             base_type_number = cache.get_or_create<pddl::TypeImpl>("number").object;
-            types_by_name.emplace("object", base_type_object);
-            types_by_name.emplace("number", base_type_number);
+            global_scope->insert("object", base_type_object);
+            global_scope->insert("number", base_type_number);
         }
     };
 }
