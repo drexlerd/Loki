bd5614d83cc9a81d717c0e7c311ab1abbf1a92eb
Dominik Drexler <dominik.drexler@gmx.net>
2024-01-04 11:03:31 +0100

    simplified boost installation

--- .github/workflows/scripts/install_boost.sh
+++ .github/workflows/scripts/install_boost.sh
@@ -2,9 +2,12 @@
 
 wget --no-check-certificate 'https://archives.boost.io/release/1.84.0/source/boost_1_84_0.tar.gz'
 tar xf boost_1_84_0.tar.gz
-cd boost_1_84_0
-./bootstrap.sh
+
+## We use header-only parts
+
+#cd boost_1_84_0
+#./bootstrap.sh
 
 # Compile with fPIC flag
-export CXXFLAGS="-fPIC"
-./b2 cxxflags="$CXXFLAGS" link=static
+#export CXXFLAGS="-fPIC"
+#./b2 cxxflags="$CXXFLAGS" link=static
