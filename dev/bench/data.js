3546cf89e3b6c786d073fe8451ded929ed0037ec
github-action-benchmark <github@users.noreply.github.com>
2024-01-07 17:29:25 +0000

    add Benchmark (googlecpp) benchmark result for 8ae8a66f6d96d4b6f3b0469306dbe60983762b99

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1704648564986,
+  "repoUrl": "https://github.com/drexlerd/Loki",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "dominik.drexler@gmx.net",
+            "name": "Dominik Drexler",
+            "username": "drexlerd"
+          },
+          "committer": {
+            "email": "dominik.drexler@gmx.net",
+            "name": "Dominik Drexler",
+            "username": "drexlerd"
+          },
+          "distinct": true,
+          "id": "8ae8a66f6d96d4b6f3b0469306dbe60983762b99",
+          "message": ".",
+          "timestamp": "2024-01-07T18:27:25+01:00",
+          "tree_id": "3ea4cb2023ec96f10f25c7a4b1d6dbd91cd619be",
+          "url": "https://github.com/drexlerd/Loki/commit/8ae8a66f6d96d4b6f3b0469306dbe60983762b99"
+        },
+        "date": 1704648564672,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "loki::benchmarks::BM_ConstructAtoms",
+            "value": 24651694.65517255,
+            "unit": "ns/iter",
+            "extra": "iterations: 29\ncpu: 24646024.137931034 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
