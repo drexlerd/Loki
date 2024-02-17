a44e2aa9a3655dcf3c19fbf3a4fa509bb5c07c97
Dominik Drexler <dominik.drexler@gmx.net>
2024-02-17 18:17:51 +0100

    added parser test

---
+++ tests/unit/domain/parser.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 Dominik Drexler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "../../../include/loki/domain/parser.hpp"
+
+#include <gtest/gtest.h>
+
+
+namespace loki::domain::tests {
+
+TEST(LokiTests, DomainParserTest) {
+    const auto domain_file = fs::path(std::string(DATA_DIR) + "gripper/domain.pddl");
+    const auto problem_file = fs::path(std::string(DATA_DIR) + "gripper/problem.pddl");
+    DomainParser parser(domain_file);
+}
+
+}
