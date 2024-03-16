666dfc820fdbc1c44e9b4ba61f7bf8ae5e014932
Dominik Drexler <dominik.drexler@gmx.net>
2024-03-16 20:57:50 +0100

    extended parser test

--- tests/unit/parser.cpp
+++ tests/unit/parser.cpp
@@ -21,11 +21,21 @@
 namespace loki::domain::tests
 {
 
-TEST(LokiTests, DomainParserTest)
+TEST(LokiTests, ParserTest)
 {
     const auto domain_file = fs::path(std::string(DATA_DIR) + "gripper/domain.pddl");
-    const auto problem_file = fs::path(std::string(DATA_DIR) + "gripper/problem.pddl");
-    DomainParser parser(domain_file);
+    const auto problem_file = fs::path(std::string(DATA_DIR) + "gripper/p-2-0.pddl");
+    auto domain_parser = DomainParser(domain_file);
+    auto problem_parser = ProblemParser(problem_file, domain_parser);
+
+    const auto domain = domain_parser.get_domain();
+    EXPECT_EQ(domain->get_constants().size(), 2);
+    EXPECT_EQ(domain->get_predicates().size(), 7);
+    EXPECT_EQ(domain->get_actions().size(), 3);
+
+    const auto problem = problem_parser.get_problem();
+    EXPECT_EQ(problem->get_objects().size(), 4);
+    EXPECT_EQ(problem->get_initial_literals().size(), 11);
 }
 
 }
