9fe8c41dac3cb4a687f7ae701973b9bb15be6554
Dominik Drexler <dominik.drexler@gmx.net>
2024-03-16 14:34:15 +0100

    fix string literals

--- src/pddl/exceptions.cpp
+++ src/pddl/exceptions.cpp
@@ -21,6 +21,10 @@
 #include "loki/pddl/function_skeleton.hpp"
 #include "loki/pddl/predicate.hpp"
 
+#include <string>
+
+using namespace std::string_literals;
+
 namespace loki
 {
 /**
@@ -28,97 +32,97 @@ namespace loki
  */
 /* Type */
 ReservedTypeError::ReservedTypeError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("Unexpected type name \"" + name + "\". It is a reserved type name", error_handler_output)
+    SemanticParserError("Unexpected type name \""s + name + "\". It is a reserved type name"s, error_handler_output)
 {
 }
 
 UnusedTypeError::UnusedTypeError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The type with name \"" + name + "\" was never referred to.", error_handler_output)
+    SemanticParserError("The type with name \""s + name + "\" was never referred to."s, error_handler_output)
 {
 }
 
 UndefinedTypeError::UndefinedTypeError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The type with name \"" + name + "\" is undefined.", error_handler_output)
+    SemanticParserError("The type with name \""s + name + "\" is undefined."s, error_handler_output)
 {
 }
 
 MultiDefinitionTypeError::MultiDefinitionTypeError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The type with name \"" + name + "\" has already been defined.", error_handler_output)
+    SemanticParserError("The type with name \""s + name + "\" has already been defined."s, error_handler_output)
 {
 }
 
 /* Predicate */
 UnusedPredicateError::UnusedPredicateError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The predicate with name \"" + name + "\" was never referred to.", error_handler_output)
+    SemanticParserError("The predicate with name \""s + name + "\" was never referred to."s, error_handler_output)
 {
 }
 
 UndefinedPredicateError::UndefinedPredicateError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The predicate with name \"" + name + "\" is undefined.", error_handler_output)
+    SemanticParserError("The predicate with name \""s + name + "\" is undefined."s, error_handler_output)
 {
 }
 
 MultiDefinitionPredicateError::MultiDefinitionPredicateError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The predicate with name \"" + name + "\" has already been defined.", error_handler_output)
+    SemanticParserError("The predicate with name \""s + name + "\" has already been defined."s, error_handler_output)
 {
 }
 
 /* Constant */
 UndefinedConstantError::UndefinedConstantError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The constant with name \"" + name + "\" is undefined.", error_handler_output)
+    SemanticParserError("The constant with name \""s + name + "\" is undefined."s, error_handler_output)
 {
 }
 
 MultiDefinitionConstantError::MultiDefinitionConstantError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The constant with name \"" + name + "\" has already been defined.", error_handler_output)
+    SemanticParserError("The constant with name \""s + name + "\" has already been defined."s, error_handler_output)
 {
 }
 
 /* Variable */
 UnusedVariableError::UnusedVariableError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The variable with name \"" + name + "\" was never referred to.", error_handler_output)
+    SemanticParserError("The variable with name \""s + name + "\" was never referred to."s, error_handler_output)
 {
 }
 
 UndefinedVariableError::UndefinedVariableError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The variable with name \"" + name + "\" is not defined in the current scope.", error_handler_output)
+    SemanticParserError("The variable with name \""s + name + "\" is not defined in the current scope."s, error_handler_output)
 {
 }
 
 MultiDefinitionVariableError::MultiDefinitionVariableError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The variable with name \"" + name + "\" has already been defined.", error_handler_output)
+    SemanticParserError("The variable with name \""s + name + "\" has already been defined."s, error_handler_output)
 {
 }
 
 /* FunctionSkeleton */
 UnusedFunctionSkeletonError::UnusedFunctionSkeletonError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The function skeleton with name \"" + name + "\" was never referred to.", error_handler_output)
+    SemanticParserError("The function skeleton with name \""s + name + "\" was never referred to."s, error_handler_output)
 {
 }
 
 UndefinedFunctionSkeletonError::UndefinedFunctionSkeletonError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The function skeleton with name \"" + name + "\" is not defined in the current scope.", error_handler_output)
+    SemanticParserError("The function skeleton with name \""s + name + "\" is not defined in the current scope."s, error_handler_output)
 {
 }
 
 MultiDefinitionFunctionSkeletonError::MultiDefinitionFunctionSkeletonError(const std::string& name, const std::string& error_handler_output) :
-    SemanticParserError("The function skeleton with name \"" + name + "\" has already been defined.", error_handler_output)
+    SemanticParserError("The function skeleton with name \""s + name + "\" has already been defined."s, error_handler_output)
 {
 }
 
 /* Requirement */
 UnusedRequirementError::UnusedRequirementError(pddl::RequirementEnum requirement, const std::string& error_handler_output) :
-    SemanticParserError("The requirement with name \"" + to_string(requirement) + "\" was never used.", error_handler_output)
+    SemanticParserError("The requirement with name \""s + to_string(requirement) + "\" was never used."s, error_handler_output)
 {
 }
 
 UndefinedRequirementError::UndefinedRequirementError(pddl::RequirementEnum requirement, const std::string& error_handler_output) :
-    SemanticParserError("Undefined requirement: " + to_string(requirement), error_handler_output)
+    SemanticParserError("Undefined requirement: "s + to_string(requirement), error_handler_output)
 {
 }
 
 UnsupportedRequirementError::UnsupportedRequirementError(pddl::RequirementEnum requirement, const std::string& error_handler_output) :
-    std::runtime_error("Unsupported requirement: " + to_string(requirement) + "\n" + error_handler_output)
+    std::runtime_error("Unsupported requirement: "s + to_string(requirement) + "\n"s + error_handler_output)
 {
 }
 
@@ -126,8 +130,8 @@ UnsupportedRequirementError::UnsupportedRequirementError(pddl::RequirementEnum r
 MismatchedPredicateTermListError::MismatchedPredicateTermListError(const pddl::Predicate& predicate,
                                                                    const pddl::TermList& term_list,
                                                                    const std::string& error_handler_output) :
-    SemanticParserError("Mismatched number of terms for predicate \"" + predicate->get_name() + "\" with sizes "
-                            + std::to_string(predicate->get_parameters().size()) + "!=" + std::to_string(term_list.size()) + ".",
+    SemanticParserError("Mismatched number of terms for predicate \""s + predicate->get_name() + "\" with sizes "s
+                            + std::to_string(predicate->get_parameters().size()) + "!="s + std::to_string(term_list.size()) + "."s,
                         error_handler_output)
 {
 }
@@ -135,8 +139,8 @@ MismatchedPredicateTermListError::MismatchedPredicateTermListError(const pddl::P
 MismatchedFunctionSkeletonTermListError::MismatchedFunctionSkeletonTermListError(const pddl::FunctionSkeleton& function_skeleton,
                                                                                  const pddl::TermList& term_list,
                                                                                  const std::string& error_handler_output) :
-    SemanticParserError("Mismatched number of terms for function skeleton \"" + function_skeleton->get_name() + "\" with sizes "
-                            + std::to_string(function_skeleton->get_parameters().size()) + "!=" + std::to_string(term_list.size()) + ".",
+    SemanticParserError("Mismatched number of terms for function skeleton \""s + function_skeleton->get_name() + "\" with sizes "s
+                            + std::to_string(function_skeleton->get_parameters().size()) + "!="s + std::to_string(term_list.size()) + ".",
                         error_handler_output)
 {
 }
