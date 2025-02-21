d3364d45be222c255cfc26e66b1b0484ee3f9b97
Dominik Drexler <dominik.drexler@gmx.net>
2025-02-21 10:59:03 +0100

    draft ProblemBuilder

--- include/loki/details/pddl/problem_builder.hpp
+++ include/loki/details/pddl/problem_builder.hpp
@@ -61,11 +61,9 @@ using ProblemTypeToRepository =
 class ProblemBuilder
 {
 private:
-    /* Memory */
-    std::shared_ptr<Domain> m_domain;
-    ProblemTypeToRepository m_repositories;
+    ProblemTypeToRepository m_repositories;  ///< Memory
+    std::shared_ptr<const Domain> m_domain;  ///< Immutable planning domain
 
-    /* Meta data */
     std::optional<fs::path> m_filepath;
     std::string m_name;
 
@@ -85,7 +83,62 @@ private:
     std::unordered_set<Axiom> m_domain_and_problem_axioms;
 
 public:
-    ProblemBuilder(std::shared_ptr<Domain> domain);
+    /// @brief Create a `ProblemBuilder` for a given `Domain`.
+    /// Copies parts elements from the `Domain` into the `ProblemTypeToRepository` and problem specific information to ensure indexing schemas.
+    /// @param domain
+    explicit ProblemBuilder(std::shared_ptr<const Domain> domain);
+
+    /// @brief Finalizes the `Problem` and returns it.
+    /// The `ProblemBuilder` is in an invalid state afterwards.
+    /// @return the resulting `Problem`.
+    Problem get_result();
+
+    ///////////////////////////////////
+    /// Child-level
+    ///////////////////////////////////
+
+    /**
+     * Unique construction.
+     */
+
+    Parameter get_or_create_parameter(Variable variable, TypeList types);
+
+    Atom get_or_create_atom(Predicate predicate, TermList terms);
+
+    FunctionExpressionNumber get_or_create_function_expression_number(double number);
+
+    FunctionExpressionBinaryOperator get_or_create_function_expression_binary_operator(BinaryOperatorEnum binary_operator,
+                                                                                       FunctionExpression left_function_expression,
+                                                                                       FunctionExpression right_function_expression);
+
+    FunctionExpressionMultiOperator get_or_create_function_expression_multi_operator(MultiOperatorEnum multi_operator,
+                                                                                     FunctionExpressionList function_expressions_);
+
+    FunctionExpressionMinus get_or_create_function_expression_minus(FunctionExpression function_expression);
+
+    FunctionExpressionFunction get_or_create_function_expression_function(Function function);
+
+    ConditionLiteral get_or_create_condition_literal(Literal literal);
+
+    ConditionAnd get_or_create_condition_and(ConditionList conditions_);
+
+    ConditionOr get_or_create_condition_or(ConditionList conditions_);
+
+    ConditionNot get_or_create_condition_not(Condition condition);
+
+    ConditionImply get_or_create_condition_imply(Condition condition_left, Condition condition_right);
+
+    ConditionExists get_or_create_condition_exists(ParameterList parameters, Condition condition);
+
+    ConditionForall get_or_create_condition_forall(ParameterList parameters, Condition condition);
+
+    ConditionNumericConstraint get_or_create_condition_numeric_constraint(BinaryComparatorEnum binary_comparator,
+                                                                          FunctionExpression function_expression_left,
+                                                                          FunctionExpression function_expression_right);
+
+    ///////////////////////////////////
+    /// Top-level
+    ///////////////////////////////////
 
     /**
      * Unique construction.
@@ -99,29 +152,43 @@ public:
 
     Literal get_or_create_literal(bool is_negated, Atom atom);
 
+    FunctionExpression get_or_create_function_expression(FunctionExpressionNumber fexpr_number);
+    FunctionExpression get_or_create_function_expression(FunctionExpressionBinaryOperator fexpr_binary_op);
+    FunctionExpression get_or_create_function_expression(FunctionExpressionMultiOperator fexpr_multi_op);
+    FunctionExpression get_or_create_function_expression(FunctionExpressionMinus fexpr_minus);
+    FunctionExpression get_or_create_function_expression(FunctionExpressionFunction fexpr_function);
     FunctionValue get_or_create_function_value(Function function, double number);
 
-    Condition get_or_create_condition();  // TODO
+    Condition get_or_create_condition(ConditionLiteral condition);
+    Condition get_or_create_condition(ConditionAnd condition);
+    Condition get_or_create_condition(ConditionOr condition);
+    Condition get_or_create_condition(ConditionNot condition);
+    Condition get_or_create_condition(ConditionImply condition);
+    Condition get_or_create_condition(ConditionExists condition);
+    Condition get_or_create_condition(ConditionForall condition);
+    Condition get_or_create_condition(ConditionNumericConstraint condition);
 
     OptimizationMetric get_or_create_optimization_metric(OptimizationMetricEnum optimization_metric, FunctionExpression function_expression);
 
     Axiom get_or_create_axiom(ParameterList parameters, Literal subtyped_literal, Condition condition);
 
     /**
-     * Problem modifiers.
+     * Top-level problem modifiers.
+     * Return true iff the element was newly inserted into the problem and false otherwise.
      */
 
-    void set_requirements(Requirements requirements);
-    void add_object(Object object);
-    void add_predicate(Predicate predicate);
-    void add_initial_literal(Literal literal);
-    void add_initial_function_value(FunctionValue function_value);
-    void set_goal_condition(Condition condition);
-    void set_optimization_metric(OptimizationMetric optimization_metric);
-    void add_axiom(Axiom axiom);
+    bool set_requirements(Requirements requirements);
+    bool add_object(Object object);
+    bool add_predicate(Predicate predicate);
+    bool add_initial_literal(Literal literal);
+    bool add_initial_function_value(FunctionValue function_value);
+    bool set_goal_condition(Condition condition);
+    bool set_optimization_metric(OptimizationMetric optimization_metric);
+    bool add_axiom(Axiom axiom);
 
     /**
-     * Problem accessors.
+     * Top-level problem accessors.
+     * Return true iff the element exist in the problem and false otherwise.
      */
 
     bool contains_requirement(RequirementEnum requirement) const;
