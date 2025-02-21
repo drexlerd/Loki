5ea90598624fbaad7e92fe3f3a06496bb1494be0
Dominik Drexler <dominik.drexler@gmx.net>
2025-02-21 12:23:50 +0100

    tmp

--- include/loki/details/pddl/problem_parser.hpp
+++ include/loki/details/pddl/problem_parser.hpp
@@ -18,11 +18,41 @@
 #ifndef LOKI_INCLUDE_LOKI_PDDL_PROBLEM_PARSER_HPP_
 #define LOKI_INCLUDE_LOKI_PDDL_PROBLEM_PARSER_HPP_
 
+#include "loki/details/pddl/context.hpp"
 #include "loki/details/pddl/declarations.hpp"
+#include "loki/details/utils/filesystem.hpp"
 
 namespace loki
 {
 
+class ProblemParser
+{
+private:
+    fs::path m_filepath;
+    // We need to keep the source in memory for error reporting.
+    std::string m_source;
+
+    // The matched positions in the input PDDL file.
+    std::unique_ptr<PDDLPositionCache> m_position_cache;
+
+    std::unique_ptr<ScopeStack> m_scopes;
+
+    // Parsed result
+    Problem m_problem;
+
+public:
+    ProblemParser(const fs::path& file_path, DomainParser& domain_parser, bool strict = false, bool quiet = true);
+    ProblemParser(const ProblemParser& other) = delete;
+    ProblemParser& operator=(const ProblemParser& other) = delete;
+    ProblemParser(ProblemParser&& other) = default;
+    ProblemParser& operator=(ProblemParser&& other) = default;
+
+    /// @brief Get position caches to be able to reference back to the input PDDL file.
+    const PDDLPositionCache& get_position_cache() const;
+
+    /// @brief Get the parsed problem.
+    const Problem& get_problem() const;
+};
 }
 
 #endif
