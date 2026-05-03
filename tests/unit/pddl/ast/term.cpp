bb06af7aef3dfcee7609a0486107c5e490cc68c8
Dominik Drexler <drexlerd93@gmail.com>
2026-05-03 15:53:02 +0200

    make loki shared

--- tests/unit/pddl/ast/term.cpp
+++ tests/unit/pddl/ast/term.cpp
@@ -15,7 +15,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "../../../src/ast/parser.hpp"
+#include "ast/parser.hpp"
 
 #include <gtest/gtest.h>
 #include <loki/ast/ast.hpp>
